e8aa5b3930c11ba68c5d912c020baa2e3e36f90b
te2u <te2u@users.noreply.github.com>
2014-09-04 23:23:16 +0900

    Silexを試してみた(4) - Twig

--- web/index.php
+++ web/index.php
@@ -5,32 +5,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Silex\Application;
 
 $app = new Application();
+
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
 $app['debug'] = true;
 
 $app->get('/', function(Application $app) {
-    $url1 = $app['url_generator']->generate('sample03-01');
-    $url2 = $app['url_generator']->generate('sample03-02', array('id' => 1));
-    $url3 = $app['url_generator']->generate('sample03-02', array('id' => 2, 'foo' => 'text'));
-
-    return <<<EOH
-<ul>
-<li><a href="$url1">sample03-01</a></li>
-<li><a href="$url2">sample03-02 01</a></li>
-<li><a href="$url3">sample03-02 02</a></li>
-</ul>
-EOH;
+    return $app['twig']->render('04/index.twig');
 });
 
-$app->get('/sample03-01', function(Application $app) {
-    return $app->escape($app['request']->getUri());
-})
-->bind('sample03-01');
+$app->get('/sample04-01', function(Application $app) {
+    return $app['twig']->render('04/sample04-01.twig', array('name' => 'Silex!!'));
+});
 
-$app->get('/sample03-02/{id}', function(Application $app) {
-    return $app->escape($app['request']->getUri());
+$app->get('/sample04-02', function(Application $app) {
+    return $app['twig']->render('04/sample04-02.twig');
 })
-->bind('sample03-02');
+->bind('sample04-02');
 
 $app->run();
