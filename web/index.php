d4ebb622dab0c5524ab89bf68724e144859040e7
te2u <te2u@users.noreply.github.com>
2014-09-10 22:40:45 +0900

    Silexを試してみた(6) - Doctrine DBAL

--- web/index.php
+++ web/index.php
@@ -2,23 +2,57 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+
 use Silex\Application;
 
 $app = new Application();
 $app['debug'] = true;
 
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__.'/../var/log/application.log',
-    'monolog.level'   => 'DEBUG',
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_sqlite',
+        'path'   => __DIR__.'/../var/data/app.db',
+    ),
 ));
 
-$app->get('/', function(Application $app) {
-    $app['monolog']->addDebug('log debug');
-    $app['monolog']->addInfo('log info');
-    $app['monolog']->addWarning('log warning');
-    $app['monolog']->addError('log warning');
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
-    return 'index';
-});
+$app->match('/', function(Application $app, Request $request) {
+
+    $name = $request->get('name');
+
+    $errors = array();
+    if ($request->isMethod('POST')) {
+
+        $pass = $request->get('pass');
+
+        $sql = 'SELECT * FROM user WHERE name = ? AND pass = ?';
+        $user = $app['db']->fetchAssoc($sql, array($name, $pass));
+
+        if (is_array($user)) {
+            return $app->redirect($app['url_generator']->generate('matched'));
+        }
+
+        $errors[] = "Invalid name or pass";
+    }
+
+    $url = $app['url_generator']->generate('index');
+
+    return $app['twig']->render('05/index.twig', array(
+        'name'   => $name,
+        'errors' => $errors,
+    ));
+})
+->method('GET|POST')
+->bind('index');
+
+$app->get('/matched', function(Application $app) {
+    return 'matched!';
+})
+->bind('matched');
 
 $app->run();
